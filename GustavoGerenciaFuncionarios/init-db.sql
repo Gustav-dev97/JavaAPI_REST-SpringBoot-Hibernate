dc8adebfb07b1f04630e38620cf9bc1c3e4f2ca3
Gustavo Batista <76456917+Gustav-dev97@users.noreply.github.com>
2024-05-31 02:11:24 -0300

    Add files docker-compose and database config

---
+++ GustavoGerenciaFuncionarios/init-db.sql
@@ -0,0 +1,10 @@
+USE crudapi;
+
+CREATE TABLE tbl_funcionario
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    genero VARCHAR(255),
+    departamento VARCHAR(255),
+    nascimento DATE
+);
